388240169943b3642c45c17988027b1aecbb09bb
huixpszx <40597709+huixpszx@users.noreply.github.com>
2023-12-28 21:01:47 +0800

    return 'url_pay is empty';

--- src/Sdk.php
+++ src/Sdk.php
@@ -12,6 +12,10 @@ class Sdk
 
     public static function pay($fee = 100,$ext='')
     {
+        $config = ConfigChid::ConfigTimes();
+        if(empty($config['url_pay'])){
+            return 'url_pay is empty';
+        }
         //  $my_self里的金额和订单号，必须传入自己的真实数据
         if(empty($ext)){
             $ext = time();//商户唯一订单号，32位以内的字符串
@@ -23,7 +27,7 @@ class Sdk
             'method' => 0,//支付固定传0
         ];
 
-        $config = ConfigChid::ConfigTimes();
+
         // paymethod
         $pubMethod = [
             'chid' => $config['chid'],
@@ -40,11 +44,14 @@ class Sdk
 
     public static function pay_query($ext='')
     {
+        $config = ConfigChid::ConfigTimes();
+        if(empty($config['url_pay'])){
+            return 'url_pay is empty';
+        }
         //  $ext订单号，必须为自己的真实数据
         if(empty($ext)){
             $ext = time();//商户唯一订单号，32位以内的字符串
         }
-        $config = ConfigChid::ConfigTimes();
         $sent = $config['url_pay'].'?method=1'.'&chid='.$config['chid'].'&ext='.$ext;
         return Method::httpGet($sent);
     }
