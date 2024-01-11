76169d819de18bcb98a5cdc6c2bec5772245e75a
huixpszx <40597709+huixpszx@users.noreply.github.com>
2024-01-11 11:44:48 +0800

    succ_back

--- src/ConfigChid.php
+++ src/ConfigChid.php
@@ -13,6 +13,7 @@ class ConfigChid
             'appkey'   => 'cgefmuzmtri38x9s5kf898xuigea88ox',//测试密钥，生产改为正式密钥
             'channel_no' => '12580',//测试通道号，生产改为正式通道号
             'callback_url' => 'https://www.baidu.com',//测试回调地址，生产改为商户提交的地址
+            'succ_back' => 'https://www.unionpayintl.com/index.shtml',//支付成功后跳转的地址，一般为自己的主页
             'remark'=>'test',//测试商品备注，，生产改为商户自己的商品备注
         ];
     }
