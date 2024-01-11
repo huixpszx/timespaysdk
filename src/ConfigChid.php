cb5706d9a776f8ddb84c1142c50b71b79a0bb628
huixpszx <40597709+huixpszx@users.noreply.github.com>
2024-01-11 13:22:01 +0800

    Update ConfigChid.php
    clothing

--- src/ConfigChid.php
+++ src/ConfigChid.php
@@ -12,9 +12,9 @@ class ConfigChid
             'chid' => 'QD101',//测试商户号，生产改为正式商户号
             'appkey'   => 'cgefmuzmtri38x9s5kf898xuigea88ox',//测试密钥，生产改为正式密钥
             'channel_no' => '12580',//测试通道号，生产改为正式通道号
-            'callback_url' => 'https://www.baidu.com',//测试回调地址，生产改为商户提交的地址
-            'succ_back' => 'https://www.unionpayintl.com/index.shtml',//支付成功后跳转的地址，一般为自己的主页
-            'remark'=>'test',//测试商品备注，，生产改为商户自己的商品备注
+            'callback_url' => 'https://www.baidu.com',//接受订单状态的，商户自己的回调地址
+            'succ_back' => 'https://www.unionpayintl.com/index.shtml',//支付成功后跳转的地址，一般为自己的产品主页
+            'remark'=>'clothing',//测试商品备注，，生产改为商户自己的商品备注
         ];
     }
 }
